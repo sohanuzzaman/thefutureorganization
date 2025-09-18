e5ed17140eed9ce46f8eb81c640bff28cb9d6d84
tanimcgs <mohan.kst.hp@gmail.com>
2025-09-18 22:25:40 +0600

    Updated next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: process.env.NODE_ENV === 'production' ? false : true,
+    unoptimized: false,
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
